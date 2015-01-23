0631c36bd6eb1a934d472f4b4bedceffc6a9dc56
kanika <kanika.midha@students.iiit.ac.in>
2015-01-24 02:45:05 +0530

    Display community page content #61

--- systers_portal/community/urls.py
+++ systers_portal/community/urls.py
@@ -2,14 +2,14 @@ from django.conf.urls import url
 
 from community.views import (EditCommunityProfileView,
                              ViewCommunityProfileView,
-                             CommunityPageView)
+                             CommunityPageView, CommunityPageListView)
 
 urlpatterns = [
     url(r'^(?P<slug>\w+)/profile/$', ViewCommunityProfileView.as_view(),
         name='view_community_profile'),
     url(r'^(?P<slug>\w+)/profile/edit/$', EditCommunityProfileView.as_view(),
         name='edit_community_profile'),
-    url(r'^(?P<slug>\w+)/$', CommunityPageView.as_view(),
+    url(r'^(?P<slug>\w+)/$', CommunityPageListView.as_view(),
         name='view_community_main_page'),
     url(r'^(?P<slug>\w+)/p/(?P<page_slug>\w+)/$',
         CommunityPageView.as_view(), name="view_community_page")
--- systers_portal/community/views.py
+++ systers_portal/community/views.py
@@ -1,12 +1,13 @@
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from django.views.generic.edit import UpdateView
 from braces.views import LoginRequiredMixin, PermissionRequiredMixin
+from django.views.generic.detail import SingleObjectMixin
 
 from community.forms import CommunityForm
 from community.mixins import CommunityMenuMixin
-from community.models import Community
+from community.models import Community, CommunityPage
 from common.mixins import UserDetailsMixin
 
 
@@ -38,10 +39,46 @@ class EditCommunityProfileView(LoginRequiredMixin, PermissionRequiredMixin,
         return request.user.has_perm("change_community", community)
 
 
+class CommunityPageListView(UserDetailsMixin, CommunityMenuMixin,
+                            SingleObjectMixin, ListView):
+    template_name = "community/page_list.html"
+    # TODO: add pagination
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Community.objects.all())
+        return super(CommunityPageListView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(CommunityPageListView, self).get_context_data(**kwargs)
+        context["community"] = self.object
+        context["news_list"] = self.object_list
+        return context
+
+    def get_queryset(self):
+        return CommunityPage.objects.filter(community=self.object)
+
+    def get_community(self):
+        """Overrides the method from CommunityMenuMixin to extract the current
+        community.
+
+        :return: Community object
+        """
+        return self.object
+
+
 class CommunityPageView(UserDetailsMixin, CommunityMenuMixin, DetailView):
     """Community page view"""
-    template_name = "community/base.html"
+    template_name = "community/page.html"
     model = Community
+
+    def get_context_data(self, **kwargs):
+        """Add Community object and News object to the context"""
+        context = super(CommunityPageView, self).get_context_data(**kwargs)
+        context["community"] = self.object
+
+        slug = self.kwargs.get('page_slug')
+        context['page'] = get_object_or_404(CommunityPage, slug=slug)
+        return context
 
     def get_community(self):
         """Overrides the method from CommunityMenuMixin to extract the current
